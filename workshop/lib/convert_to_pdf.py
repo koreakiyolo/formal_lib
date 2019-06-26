e4e0ff10e38ea0c409196b0aff804c75ad01135b
haruyuki <oda.haruyuki.44n@gmail.com>
2019-06-26 21:48:27 +0900

    commit

--- workshop/lib/convert_to_pdf.py
+++ workshop/lib/convert_to_pdf.py
@@ -4,20 +4,30 @@
 import img2pdf
 import os
 # my lib
-from help_os import ConfirmExt
 
 
 class ConfirmExt(object):
     def __init__(self, fpath_ext=".png"):
         self.fpath_ext = fpath_ext
-        
+
     def __call__(self, fnm):
-        basenm, ext = os.path.splitext(
-                                    fpath_ext)
+        basenm, ext = os.path.splitext(fnm)
         if ext != self.fpath_ext:
             emes = "{} has invalid extention:".format(fnm)
             raise TypeError(emes)
         return fnm
+
+
+class Filterext(object):
+    def __init__(self, ext=".png"):
+        self.ext_base = ext
+
+    def __call__(self, files):
+        files_li = []
+        for fpath in files:
+            if fpath.startwith(self.ext_base):
+                files_li.append(fpath)
+        return files_li
 
 
 class ConverterToPdf(object):
@@ -25,16 +35,20 @@ class ConverterToPdf(object):
         self.png_list = []
         self.confirmer = ConfirmExt(".png")
 
-
     def add_png(self, *pngs):
         for png in pngs:
             png = self.confirmer(png)
             self.png_list.append(png)
 
-    def add_png_from_dir(self):
-        if 
+    def add_png_from_dir(self, dpath):
+        if not os.path.exists(dpath):
+            emes = "{} is not directory"
+            emes = emes.format(dpath)
+            raise TypeError(emes)
+        for fnm in os.listdir(dpath):
+            fpath = os.path.exists(dpath, fnm)
+            self.png_list.append(fpath)
 
-    
     def _gene_converter(self):
         for png in self.png_list:
             info = img2pdf.convert(png)
