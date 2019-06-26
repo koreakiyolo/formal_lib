a4b27e1bd4203a0ab873db697b440f0418576275
haruyuki <oda.haruyuki.44n@gmail.com>
2019-06-26 22:00:41 +0900

    commit

--- workshop/bin/convert_to_pdf.py
+++ workshop/bin/convert_to_pdf.py
@@ -1,12 +1,36 @@
-#!/usr.bine/env python3
+#!/usr/bin/env python3
 
 # formal lib
 import os
 # my lib
-from convert_to_pdf import ConverterToPdf
-from help_argparse import fnmstr
-from help_argparse import dirstr
-from help_argparse import print_global_varibales
+from change_png_to_pdf import ConverterToPdf
+
+
+def print_global_varibales():
+    glva_dict = globals()
+    print_dict = {key: va for key, va in glva_dict
+                  if key.isupper()}
+    print("== global varibale is in the following. ==")
+    print(print_dict)
+    print("==========================================")
+
+
+def fnmstr(path_str):
+    if not os.path.exists(path_str):
+        mes = "{} is not file.".format(
+                                    path_str
+                                      )
+        raise OSError(mes)
+    return path_str
+
+
+def dirstr(path_str):
+    if not os.path.isdir(path_str):
+        mes = "{} is not directory.".format(
+                                        path_str
+                                           )
+        raise OSError(mes)
+    return path_str
 
 
 if __name__ == "__main__":
@@ -18,7 +42,7 @@ if __name__ == "__main__":
     parser.add_argument("png_files", type=fnmstr, nargs="*")
     parser.add_argument("--png_dir", type=dirstr, nargs="?", default=None)
     parser.add_argument("--out_pdf", type=str, nargs="?",
-                        requried=True)
+                        required=True)
     args = parser.parse_args()
     PNG_FILES = args.png_files
     PNG_DIR = args.png_dir
@@ -30,5 +54,4 @@ if __name__ == "__main__":
                                     PNG_DIR)
     else:
         converter_ins.add_png(PNG_FILES)
-    converter_ins.
-
+    converter_ins.to_pdf(OUT_PDF)
