4de1da50f48bcab190214c00b4128cb4868cfdcd
haruyuki <oda.haruyuki.44n@gmail.com>
2020-05-17 15:53:40 +0900

    comit

--- my_setting/vim_neobundle/setup_neobunle.zsh
+++ my_setting/vim_neobundle/setup_neobunle.zsh
@@ -2,7 +2,9 @@
 
 mkdir -p ~/.vim/bundle
 # NeoBundleをリポジトリから取得
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 mkdir -p ~/.vim/after/ftplugin/
 cwd=`dirname ${0}`
 cp ${cwd}/fortran.vim ~/.vim/after/ftplugin/
+cp ~/.vimrc ~/.vimrc_old
+cp ./vimrc_bundle ~/.vimrc
