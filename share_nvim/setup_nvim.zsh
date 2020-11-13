d8d380495e2b9defb9feb4d2d4ef29d5356a1f80
haruyuki <oda.haruyuki.44n@gmail.com>
2020-11-13 19:44:49 +0900

    comit

--- share_nvim/setup_nvim.zsh
+++ share_nvim/setup_nvim.zsh
@@ -4,10 +4,10 @@
 cwd=`dirname "${0}"`
 cd ${cwd}
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-sh ./install.sh ~/.config/nvim
+sh ./installer.sh ~/.config/nvim
 mkdir -p ~/.config/nvim
 echo 'XDG_BASE_HOME=${HOME}:./config' >> ~/.zshrc
 source ~/.zshrc
-cp ./init.vim ~/.config/nvim/
+sed -e "s@<HOME>@${HOME}@g" init.vim >> ~/.config/nvim/init.vim
 cp ./dein.toml ~/.config/nvim/
 cp ./dein_lazy.toml ~/.config/nvim/
