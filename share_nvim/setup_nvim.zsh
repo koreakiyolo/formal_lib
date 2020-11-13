85534191f29da4d23d69f97c08a03a5f0cd5afeb
haruyuki <oda.haruyuki.44n@gmail.com>
2020-11-13 18:44:01 +0900

    comit

--- share_nvim/setup_nvim.zsh
+++ share_nvim/setup_nvim.zsh
@@ -7,3 +7,5 @@ mkdir -p ~/.config/nvim
 echo 'XDG_BASE_HOME=${HOME}:./config' >> ~/.zshrc
 source ~/.zshrc
 cp ./init.vim ~/.config/nvim/
+cp ./dein.toml ~/.config/nvim/
+cp ./dein_lazy.toml ~/.config/nvim/
